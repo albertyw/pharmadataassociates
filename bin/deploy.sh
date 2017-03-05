8f0735c82de6d31a2e6dc9b62f2a3ec2bf81aff4
Albert Wang <albertyw@mit.edu>
2017-03-04 22:29:50 -0800

    Update to baseflask v1.2.1

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,6 +10,10 @@ source `which virtualenvwrapper.sh`
 workon pharmadataassociates
 pip install -r requirements.txt
 
+# Make generated static file directory writable
+sudo chown www-data app/static/gen
+sudo chown www-data app/static/.webassets-cache
+
 # Restart services
 sudo service nginx restart
 sudo service uwsgi restart
