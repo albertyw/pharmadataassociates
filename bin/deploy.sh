29e9e70588148863319ac2cbf151c064f96724b8
Albert Wang <git@albertyw.com>
2019-12-29 00:37:34 +0800

    Update to base-flask v3.5.5

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -18,6 +18,7 @@ if [ "$ENV" = "production" ]; then
 fi
 
 # Build and start container
+docker pull "$(grep FROM Dockerfile | awk '{print $2}')"
 docker build -t "pharmadataassociates:$ENV" .
 docker network inspect "pharmadataassociates" &>/dev/null ||
     docker network create --driver bridge "pharmadataassociates"
