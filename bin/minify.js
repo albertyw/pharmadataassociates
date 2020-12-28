fd3c59afa1fba1d8fcd07da98de11fb0e68e9ee3
Albert Wang <git@albertyw.com>
2020-12-27 20:24:30 -0800

    Update to base-flask v3.7.4

--- bin/minify.js
+++ bin/minify.js
@@ -6,21 +6,26 @@ const CleanCSS = require('clean-css');
 require('dotenv').config();
 
 // CSS Minification Configs
-const cssSources = [
+let cssSources = [
   path.join('node_modules', 'normalize.css', 'normalize.css'),
   path.join('node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
-  path.join('static', 'css', 'global.css'),
 ];
+let customCSS = fs.readdirSync(path.join('static', 'css'));
+customCSS = customCSS.map((f) => path.join('static', 'css', f));
+cssSources = cssSources.concat(customCSS);
 const cssOutputFile = path.join('static', 'gen', 'bundle.min.css');
 
 // JS Minification Configs
 const jsInputFile = path.join('static', 'js', 'index.js');
 const jsOutputFile = path.join('static', 'gen', 'bundle.min.js');
 const jsRawAppends = [
-  path.join('node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.min.js')
+  path.join('node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.min.js'),
 ];
 
 // Minify js
+console.log('Minifying JS:');
+console.log(jsInputFile);
+console.log('');
 const jsOutputStream = fs.createWriteStream(jsOutputFile);
 browserify(jsInputFile, {debug: true})
   .transform('envify')
@@ -44,6 +49,8 @@ jsOutputStream.on('finish', () => {
 
 
 // Minify CSS
+console.log('Minifying CSS:');
+console.log(cssSources.join(', '));
 new CleanCSS({returnPromise: true})
   .minify(cssSources)
   .then(output => {
