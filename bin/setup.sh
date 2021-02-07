c0ceb164edd743071d7049a6cf62da950cbecfe0
Albert Wang <git@albertyw.com>
2021-02-06 21:31:42 -0800

    Simplify setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,37 +8,9 @@
 set -exuo pipefail
 IFS=$'\n\t'
 
-# Setup server
-sudo hostnamectl set-hostname "$HOSTNAME"
-
 # Clone repository
 cd ~
-git clone git@github.com:albertyw/pharmadataassociates
-
-# Install nginx
-sudo add-apt-repository ppa:nginx/stable
-sudo apt-get update
-sudo apt-get install -y nginx
-
-# Configure nginx
-sudo rm -rf /etc/nginx/sites-available
-sudo rm -rf /etc/nginx/sites-enabled/*
-sudo ln -s ~/pharmadataassociates/config/nginx/app /etc/nginx/sites-enabled/pharmadataassociates-app
-sudo rm -rf /var/www/html
-
-# Secure nginx
-sudo mkdir -p /etc/nginx/ssl
-curl https://ssl-config.mozilla.org/ffdhe2048.txt | sudo tee /etc/nginx/ssl/dhparams.pem > /dev/null
-# Copy server.key and server.pem to /etc/nginx/ssl.  The privatey/public key
-# pair can be generated from Cloudflare or letsencrypt.
-sudo service nginx restart
-
-# Set up docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker "${USER}"
+git clone "git@github.com:albertyw/pharmadataassociates"
 
 # Set up directory structures
 ln -s .env.production .env
