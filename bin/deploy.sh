c337a93387b7cbdbc29db924de7fef1dbe581145
Albert Wang <git@albertyw.com>
2021-06-08 23:04:59 -0700

    Update to base-flask v3.10.3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -51,5 +51,5 @@ if [ "$ENV" = "production" ]; then
 
     # Update nginx
     sudo cp "/home/albertyw/pharmadataassociates/config/nginx/app" "/etc/nginx/sites-enabled/pharmadataassociates-app"
-    sudo service nginx reload
+    docker container restart nginx
 fi
